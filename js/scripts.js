1f4eb0105569f3b559918692b8ae5160d608ee12
raschkatie <raschkatie@gmail.com>
2024-03-22 12:28:31 -0400

    Changed code according to Exercise 1.2
    chose 3 different pokemon and defined their name, height and types

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = 'fettucine alfredo';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'eevee', height: 2, types: ['normal', 'electric']}, 
+    {name: 'vulpix', height: 4, types: ['fire', 'dark']}, 
+    {name: 'gardevoir', height: 10, type: ['psychic', 'fairy']}
+];
